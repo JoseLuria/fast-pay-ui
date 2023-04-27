aefc878e6c3b962291751d8a50288bb546e463bb
JoseLuria <jos02-log18@hotmail.com>
2023-04-27 09:19:36 -0500

    feat: :lipstick: created button component

---
+++ src/shared/Button.tsx
@@ -0,0 +1,45 @@
+import type { ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+const styles = {
+  base: 'px-6 text-sm font-semibold h-12 flex text-center items-center justify-center rounded-3xl duration-200 disabled:bg-dark-gray disabled:text-white disabled:cursor-not-allowed',
+  outline:
+    'border-2 border-dark-gray text-white hover:bg-white hover:text-black focus-visible:bg-white focus-visible:text-black',
+  error: 'text-white bg-red hover:bg-low-red focus-visible:bg-low-red',
+  warn: 'text-white bg-orange hover:bg-low-orange focus-visible:bg-low-orange',
+  success: 'text-white bg-green hover:bg-low-green focus-visible:bg-low-green',
+  default:
+    'text-black bg-white hover:bg-low-gray hover:text-white focus-visible:bg-low-gray focus-visible:text-white'
+}
+interface StyleProps {
+  full?: boolean
+  variant?: 'default' | 'outline' | 'warn' | 'error' | 'success'
+}
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  href?: undefined
+}
+
+type AnchorProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
+  href: string
+}
+
+const hasHref = (props: ButtonProps | AnchorProps): props is AnchorProps => 'href' in props
+
+interface Overload {
+  (props: ButtonProps & StyleProps): JSX.Element
+  (props: AnchorProps & StyleProps): JSX.Element
+}
+
+export const Button: Overload = ({ className, variant = 'default', full, ...props }) => {
+  const btnStyles = clsx(styles.base, styles[variant], full && 'w-full', className)
+
+  const componentProps = { className: btnStyles, ...props }
+
+  if (hasHref(componentProps)) {
+    return <Link {...componentProps} href={componentProps.href} />
+  }
+
+  return <button {...componentProps} />
+}
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Button } from '@/shared'
+
+const meta: Meta<typeof Button> = {
+  title: 'Shared/Button',
+  component: Button
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
+
+const children = 'Hello'
+
+export const Default: Story = {
+  args: {
+    children,
+    variant: 'default'
+  }
+}
+
+export const Outline: Story = {
+  args: {
+    children,
+    variant: 'outline'
+  }
+}
+
+export const Warn: Story = {
+  args: {
+    children,
+    variant: 'warn'
+  }
+}
+
+export const Error: Story = {
+  args: {
+    children,
+    variant: 'error'
+  }
+}
+
+export const Success: Story = {
+  args: {
+    children,
+    variant: 'success'
+  }
+}
