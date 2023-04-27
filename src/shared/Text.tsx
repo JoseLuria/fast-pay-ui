95506ffd01ee20b8c4071e05553ca087794cb8a1
JoseLuria <jos02-log18@hotmail.com>
2023-04-27 09:20:21 -0500

    feat: :lipstick: created text component

---
+++ src/shared/Text.tsx
@@ -0,0 +1,18 @@
+import type { FC, ReactNode } from 'react'
+import clsx from 'clsx'
+
+interface Props {
+  tag?: 'h1' | 'h2' | 'p'
+  children?: ReactNode
+  className?: string
+}
+
+const styles = {
+  h1: 'text-2xl text-white font-semibold md:text-3xl',
+  h2: 'font-semibold text-white text-xl',
+  p: 'text-sm text-white text-opacity-50'
+}
+
+export const Text: FC<Props> = ({ tag: Tag = 'p', children, className }) => {
+  return <Tag className={clsx(styles[Tag], className)}>{children}</Tag>
+}
---
+++ src/stories/Text.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text } from '@/shared'
+
+const meta: Meta<typeof Text> = {
+  title: 'Shared/Text',
+  component: Text
+}
+
+export default meta
+
+type Story = StoryObj<typeof Text>
+
+export const Paragraph: Story = {
+  args: {
+    children: 'This is a paragraph',
+    tag: 'p'
+  }
+}
+
+export const HeadingOne: Story = {
+  args: {
+    children: 'This is a heading one',
+    tag: 'h1'
+  }
+}
+
+export const HeadingTwo: Story = {
+  args: {
+    children: 'This is a heading two',
+    tag: 'h2'
+  }
+}
