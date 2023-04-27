ef1cef6e6b9b7e7b41a45d0ba5d8e26d78261ba8
JoseLuria <jos02-log18@hotmail.com>
2023-04-27 09:18:48 -0500

    feat: :lipstick: added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: '#ECEFF4',
+      black: '#131313',
+      'dark-gray': '#292929',
+      'low-gray': '#535353',
+      gray: '#949494',
+      green: '#1CDB9B',
+      'low-green': '#75E0BC',
+      orange: '#FF8F00',
+      'low-orange': '#E7AE66',
+      red: '#E74C3C',
+      'low-red': '#FF9797'
+    },
     extend: {}
   },
   plugins: []
