c073c273388aaeaa538266e1905aa65929e4b25f
JoseLuria <jos02-log18@hotmail.com>
2023-05-02 14:38:56 -0500

    feat: :sparkles: added html types to text

--- src/shared/Text.tsx
+++ src/shared/Text.tsx
@@ -1,11 +1,23 @@
-import type { FC, ReactNode } from 'react'
+import type { HTMLAttributes } from 'react'
 import clsx from 'clsx'
 
-interface Props {
-  tag?: 'h1' | 'h2' | 'p'
-  children?: ReactNode
-  className?: string
+type TextTag = 'h1' | 'h2' | 'p'
+
+interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
+  tag?: TextTag
 }
+
+interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
+  tag?: TextTag
+}
+
+interface Overload {
+  (props: ParagraphProps): JSX.Element
+  (props: HeadingProps): JSX.Element
+}
+
+const isParagraph = (props: ParagraphProps | HeadingProps): props is ParagraphProps =>
+  props.tag === 'p'
 
 const styles = {
   h1: 'text-2xl text-white font-semibold md:text-3xl',
@@ -13,8 +25,17 @@ const styles = {
   p: 'text-sm text-white text-opacity-50'
 }
 
-const Text: FC<Props> = ({ tag: Tag = 'p', children, className }) => {
-  return <Tag className={clsx(styles[Tag], className)}>{children}</Tag>
+const Text: Overload = ({ tag: Tag = 'p', className, ...props }) => {
+  const tagProps = {
+    className: clsx(styles[Tag], className),
+    ...props
+  }
+
+  if (isParagraph({ ...tagProps, tag: Tag })) {
+    return <p {...tagProps} />
+  }
+
+  return <Tag {...tagProps} />
 }
 
 export default Text
