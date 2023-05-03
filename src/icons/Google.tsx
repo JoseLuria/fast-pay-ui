9061a97d47a523de5fc645b7d5f9339ebaded567
JoseLuria <jos02-log18@hotmail.com>
2023-05-03 15:56:46 -0500

    feat: :sparkles: added global react

--- src/icons/Google.tsx
+++ src/icons/Google.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { SVGProps } from 'react'
 
 const Google = (props: SVGProps<SVGSVGElement>) => (
--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import type { ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
 import Link from 'next/link'
 import clsx from 'clsx'
